f51644ccaa6966746b89d5ec92912435c560e254
ykp208 <ykp208@naver.com>
2018-08-03 23:25:20 +0900

    1.0.3

---
+++ src/hashTable/HashTable_01.java
@@ -0,0 +1,104 @@
+package hashTable;
+
+import java.util.LinkedList;
+
+class HashTable{
+	class Node{
+		String key;
+		String value;
+		
+		public Node(String key,String value) {
+			this.key =key;
+			this.value =value;
+		}
+		String value() {
+			return value;
+		}
+		void value(String value) {
+			this.value =value;
+		}
+		
+	}
+	LinkedList<Node>[] data;
+	HashTable(int size){
+		this.data = new LinkedList[size];
+	}
+	int getHashCode(String key) {
+		int hashcode = 0;
+		for(char c: key.toCharArray()) {
+			hashcode +=c;
+		}
+		return hashcode;
+	}
+	int convertToIndex(int hashcode) {
+		return hashcode % data.length;
+	}
+	Node searchKey(LinkedList<Node> list, String key) {
+		if(list==null) return null;
+		for(Node node: list) {
+			if(node.key.equals(key)) {
+				return node;
+			}
+		}
+		return null;
+	}
+	void put(String key,String value) {
+		int hashcode = getHashCode(key);
+		int index = convertToIndex(hashcode);
+		LinkedList<Node> list = data[index];
+		if(list == null) {
+			list = new LinkedList<Node>();
+			data[index] = list;
+		}
+		Node node =searchKey(list, key);
+		if(node == null) {
+			list.addLast(new Node(key, value));
+		}else {
+			node.value(value);
+		}
+	}
+	String get(String key) {
+		int hashcode = getHashCode(key);
+		int index =convertToIndex(hashcode);
+		LinkedList<Node> list = data[index];
+		Node node = searchKey(list, key);
+		return node == null? "Not found" : node.value();
+		
+	}
+}
+
+
+public class HashTable_01 {
+	//o n 까지 검색시간걸릴수 있음
+	//collison 한방에  많은값이 들거가버리면
+	public static void main(String[] args) {
+	
+
+		HashTable h = new HashTable(3);
+		h.put("sung", "she is pretty");
+		h.put("jin", "she is a model");
+		h.put("hee", "she is an angel");
+		h.put("min", "she is cute");
+		h.put("min1", "she is cute");
+		System.out.println(h.get("sung"));
+		System.out.println(h.get("jin"));
+		System.out.println(h.get("hee"));
+		System.out.println(h.get("min"));
+		System.out.println(h.get("min1"));
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
